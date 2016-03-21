647d81821e179bbbe048b2f835ff5c1326eacc50
zjh-neverstop <zhaojinghao1986@126.com>
2016-03-21 15:17:40 +0800

    add ConnectionPool

---
+++ Common/ConnectionConfig.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ConnectionConfig
+    {
+        /// <summary>
+        /// Dictionary of client properties to be sent to the server.
+        /// </summary>
+        public IDictionary<String, object> ClientProperties { get; set; }
+
+        /// <summary>
+        /// Password to use when authenticating to the server.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Maximum channel number to ask for.
+        /// </summary>
+        public ushort RequestedChannelMax { get; set; }
+
+        /// <summary>
+        /// Frame-max parameter to ask for (in bytes).
+        /// </summary>
+        public uint RequestedFrameMax { get; set; }
+
+        /// <summary>
+        /// Heartbeat setting to request (in seconds).
+        /// </summary>
+        public ushort RequestedHeartbeat { get; set; }
+
+        /// <summary>
+        /// When set to true, background threads will be used for I/O and heartbeats.
+        /// </summary>
+        public bool UseBackgroundThreadsForIO { get; set; }
+
+        public string HostName { get; set; }
+
+        /// <summary>
+        /// Username to use when authenticating to the server.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Virtual host to access during this connection.
+        /// </summary>
+        public string VirtualHost { get; set; }
+
+        /// <summary>
+        /// Advanced option.
+        /// 
+        /// What task scheduler should consumer dispatcher use.
+        /// </summary>
+        public TaskScheduler TaskScheduler { get; set; }
+
+        /// <summary>
+        /// Amount of time protocol handshake operations are allowed to take before
+        /// timing out.
+        /// </summary>
+        public TimeSpan HandshakeContinuationTimeout { get; set; }
+
+        /// <summary>
+        /// Amount of time protocol  operations (e.g. <code>queue.declare</code>) are allowed to take before
+        /// timing out.
+        /// </summary>
+        public TimeSpan ContinuationTimeout { get; set; }
+    }
+}
---
+++ Common/ConnectionPool.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Common
+{
+    /// <summary>
+    /// 连接池，因为RabbitMQ的连接对象可以共用，所以从池中获取对象的时候仅仅只是返回连接对象的引用
+    /// </summary>
+    public class ConnectionPool
+    {
+        private static int _index = -1;
+
+        private static ConnectionFactory _factory;
+
+        public static int _size = 20;  //默认创建20条TCP连接
+
+        public static ConnectionConfig Config;
+
+        private static IConnection[] _connPool = new IConnection[_size];
+
+        private ConnectionPool() { }
+
+        static ConnectionPool()
+        {
+            _factory = new ConnectionFactory()
+            {
+                HostName = Config.HostName ?? "localhost",
+                VirtualHost = Config.VirtualHost ?? "/",
+                UserName = Config.UserName ?? "guest",
+                Password = Config.Password ?? "guest",
+                RequestedHeartbeat = 60,
+                Port = AmqpTcpEndpoint.UseDefaultPort,
+                AutomaticRecoveryEnabled = true,
+                TopologyRecoveryEnabled = true
+            };
+        }
+
+        /// <summary>
+        /// 初始化池对象
+        /// </summary>
+        public static void Init()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                _connPool[i] = _factory.CreateConnection();
+            }
+        }
+
+        /// <summary>
+        /// 从池中获取连接
+        /// </summary>
+        /// <returns></returns>
+        public static IConnection GetConnection()
+        {
+            int index, computedIndex;
+            do
+            {
+                index = _index;
+                computedIndex = (index + 1) % _size;
+            } while (_index != Interlocked.CompareExchange(ref _index, computedIndex, index));
+            return _connPool[computedIndex];
+        }
+    }
+}
